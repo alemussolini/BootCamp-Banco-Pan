e5a08a4f9da445f63a213558bba658ebbca7e219
alemussolini <alessandromussolini@hotmail.com>
2023-02-18 20:07:07 -0300

    sintaxe java

---
+++ Sintaxe Java/java-sintaxe/src/edu/alessandro/sintaxeJava/MinhaClasse.java
@@ -0,0 +1,36 @@
+package edu.alessandro.sintaxeJava;
+public class MinhaClasse {
+    
+    public static void main (String [] args ){
+
+        String primeiroNome = "Alessandro";
+        String segundoNome = "Mussolini";
+
+        String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+
+        System.out.println(nomeCompleto);
+
+       /*  System.out.println("Hello World"); */
+
+       /* String meuNome = "Alessandro";
+          int anoFabricacao = 2022;
+          boolean verdadeira = true;
+          anoFabricacao = 2018;
+        */
+    }
+       
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        
+        /* METODOS
+         * ESTRUTURA
+         * TipoRetorno NomeObjetivoNoInfinito Parametro(s)
+         * EXEMPLO
+         * int somar (int numeroUm, int numero2)
+         * String formatarCep (long cep)
+         */
+
+             return primeiroNome.concat(" ").concat(segundoNome);
+         }
+    
+}
