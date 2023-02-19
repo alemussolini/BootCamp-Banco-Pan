8f0f01918c705c29bf9636c5f55340b34381760f
alemussolini <alessandromussolini@hotmail.com>
2023-02-18 22:24:02 -0300

    organizacao

---
+++ Tipos e Variáveis/src/edu/alessandro/tiposevariaveis/TiposVariaveis.java
@@ -0,0 +1,28 @@
+package edu.alessandro.tiposevariaveis;
+
+public class TiposVariaveis {
+
+    public static void main(String[] args) {
+        
+        /* COMO INDICAR VARIAVEIS
+         *  int idade; // Tipo "int", nome "idade", nenhuma valor atribuido
+         *  int anoFabricacao = 2021; // Tipo "int", nome "anoFabricacao", com valor atribuido
+         *  double salarioMinimo = 2500; // Tipo "double", nome "salarioMinimo", com valor atribuido
+         */
+
+         /* public class TipoDados {
+                public static void main(String [] args){
+                    byte idade = 123;
+                    short ano = 2023;
+                    int cep = 23812525; // se começar com 0 talves tenha que ser de outro tipo.
+                    long cpf = 12345678912L; // se começar com 0 talves tenha que ser de outro tipo. precisa terminar com L ou l.
+                    float pi = 3.14F; // tipo float deve ter o F ou f no final.
+                    double salario = 2301.54;
+                }
+         }
+          * 
+          */
+
+    }
+
+}
